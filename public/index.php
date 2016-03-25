53088be3c31f15a180ef9e2d09e2a8b05a322534
Pavel Volyntsev <pavel@visualpharm.com>
2016-03-25 11:52:32 +0600

    Simple styles

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Diglot</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
+<!--    <script type="text/javascript" src="/js/app.js"></script>-->
+</head>
+<body>
+<div class="page-wrapper">
+    <h1><?php echo "Diglot project" ?></h1>
 
-echo 'It works!';
+    <ul>
+        <li><a class="language-primary" href="/book/97things-dev">97 Things Every Programmer Should Know <span class="label green">english</span></a>
+            <a class="language-secondary" href="/book/97things-dev">97 вещей, которые должен знать каждый программист <span class="label blue">русский</span></a>
+        </li>
+
+        <li><a class="language-primary" href="/article/write-code-every-day">Write Code Every Day <span class="label green">english</span></a>
+            <a class="language-secondary" href="/article/write-code-every-day">Пишите код каждый день <span class="label blue">русский</span></a>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
