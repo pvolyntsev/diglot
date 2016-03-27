ba8af48859ecc9cb371af197051dc68ef531d40b
Pavel Volyntsev <pavel@visualpharm.com>
2016-03-28 00:45:52 +0600

    Changed main navigation menu

--- public/index.php
+++ public/index.php
@@ -29,16 +29,12 @@
 <!--    <script type="text/javascript" src="/js/app.js"></script>-->
 </head>
 <body>
-<div class="page-wrapper">
-    <h1><?php echo "Diglot project" ?></h1>
+<div class="page home">
+    <h1 class="title"><?php echo "Diglot project" ?></h1>
 
-    <ul>
-        <li><a class="language-primary" href="/book/97things-dev">97 Things Every Programmer Should Know <span class="label green">english</span></a>
-            <a class="language-secondary" href="/book/97things-dev">97 вещей, которые должен знать каждый программист <span class="label blue">русский</span></a>
-        </li>
-
-        <li><a class="language-primary" href="/article/write-code-every-day">Write Code Every Day <span class="label green">english</span></a>
-            <a class="language-secondary" href="/article/write-code-every-day">Пишите код каждый день <span class="label blue">русский</span></a>
+    <ul class="navigation">
+        <li><a class="first" href="article.php?article=write-code-every-day">Write Code Every Day<span class="label green">english</span></a>
+            <a class="second" href="article.php?article=write-code-every-day">Пишите код каждый день<span class="label blue">русский</span></a>
         </li>
     </ul>
 </div>
