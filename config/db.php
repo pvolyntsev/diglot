5202c8ed85fce5f0afe833d45685936862dce94c
tobeinword1987 <tobeinword1987@gmail.com>
2016-05-04 20:34:32 +0300

    [change] add yii2 scelet

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=phpmyadmin',
+    'username' => 'pma',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
