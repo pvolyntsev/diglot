e2521cd2d1035e94d2a7adb77ebefb4b378e3df8
Pavel Volyntsev <pavel@visualpharm.com>
2016-05-09 03:03:21 +0600

    [change] changed database user and database name

--- config/db.php
+++ config/db.php
@@ -1,9 +1,15 @@
 <?php
 
+/**
+DROP DATABASE IF EXISTS `diglot`;
+CREATE DATABASE IF NOT EXISTS `diglot` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+GRANT ALL PRIVILEGES ON diglot.* TO diglot_rw@localhost IDENTIFIED BY '123456';
+*/
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=phpmyadmin',
-    'username' => 'pma',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=diglot',
+    'username' => 'diglot_rw',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
