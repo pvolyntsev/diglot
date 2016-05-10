3833bfd0555cacabee70f6f8a3868c45d2df8c8c
Pavel Volyntsev <pavel@visualpharm.com>
2016-05-11 00:21:46 +0600

    [added] добавил локальные IP адреса для виртуальных машин (192.168.*.*)

--- config/web.php
+++ config/web.php
@@ -140,6 +140,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => [ '127.0.0.1', '::1', '192.168.*.*' ]
     ];
 }
 
