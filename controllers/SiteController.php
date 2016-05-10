5bde40fc5024fb172a4cc87eef0b5137d56b2a9b
Pavel Volyntsev <pavel@visualpharm.com>
2016-05-10 23:43:37 +0600

    [deleted] removed old login page

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -52,28 +52,6 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
-
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
-
     public function actionContact()
     {
         $model = new ContactForm();
