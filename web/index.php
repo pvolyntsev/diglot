45984c21dd612cbad6080e27f6948f335ef2f934
Pavel Volyntsev <pavel@visualpharm.com>
2016-05-11 19:30:35 +0600

    [added] added local environment configuration file

--- web/index.php
+++ web/index.php
@@ -1,9 +1,11 @@
 <?php
 
+if (is_file(__DIR__.'/environment.local.php'))
+    include(__DIR__.'/environment.local.php');
 
-// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', true);
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
