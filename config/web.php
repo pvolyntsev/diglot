0d3826cf3adabd3b60a67ddfc2144b2608b5d31f
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-05-13 03:33:30 +0600

    [added] добавил локальные IP адреса для виртуальных машин (192.168.*.*)

--- config/web.php
+++ config/web.php
@@ -142,6 +142,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => [ '127.0.0.1', '::1', '192.168.*.*' ],
     ];
 
     $config['bootstrap'][] = 'gii';
