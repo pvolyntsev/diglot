65397c1a1abe18d260f07d2e8139339199be2011
Nikolay <n.poymanov@gmail.com>
2016-05-14 22:45:36 +0300

    [added] move github toneks to local params
    Токены github перенесены в params.php.local
    params.php.local добавлен в .gitignore

--- config/params.php
+++ config/params.php
@@ -18,49 +18,4 @@ return [
         'encryption' => null,
     ],
 
-    // List of options to authorize using third-party services
-    'authClientCollection.clients' => [
-//        'vkontakte' => [
-//            'class' => 'budyaga\users\components\oauth\VKontakte',
-//            'clientId' => 'XXX',
-//            'clientSecret' => 'XXX',
-//            'scope' => 'email'
-//        ],
-//        'google' => [
-//            'class' => 'budyaga\users\components\oauth\Google',
-//            'clientId' => 'XXX',
-//            'clientSecret' => 'XXX',
-//        ],
-//        'facebook' => [
-//            'class' => 'budyaga\users\components\oauth\Facebook',
-//            'clientId' => 'XXX',
-//            'clientSecret' => 'XXX',
-//        ],
-//        'github' => [
-//            'class' => 'budyaga\users\components\oauth\GitHub',
-//            'clientId' => 'XXX',
-//            'clientSecret' => 'XXX',
-//            'scope' => 'user:email, user'
-//        ],
-//        'linkedin' => [
-//            'class' => 'budyaga\users\components\oauth\LinkedIn',
-//            'clientId' => 'XXX',
-//            'clientSecret' => 'XXX',
-//        ],
-//        'live' => [
-//            'class' => 'budyaga\users\components\oauth\Live',
-//            'clientId' => 'XXX',
-//            'clientSecret' => 'XXX',
-//        ],
-//        'yandex' => [
-//            'class' => 'budyaga\users\components\oauth\Yandex',
-//            'clientId' => 'XXX',
-//            'clientSecret' => 'XXX',
-//        ],
-//        'twitter' => [
-//            'class' => 'budyaga\users\components\oauth\Twitter',
-//            'consumerKey' => 'XXX',
-//            'consumerSecret' => 'XXX',
-//        ],
-    ]
 ];
