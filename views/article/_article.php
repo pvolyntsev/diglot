7049758ebc0c99776cceddffb761aa2dd1e26906
tobeinword1987 <tobeinword1987@gmail.com>
2016-05-19 21:45:04 +0300

    1

--- views/article/_article.php
+++ views/article/_article.php
@@ -2,7 +2,10 @@
 use yii\helpers\Html;
 use yii\widgets\ListView;
 use app\models\Article;
+use app\models\Language;
 
+$article=new Article();
+$language=new \app\models\Language();
 ?>
 
 	<div class="article">
@@ -10,13 +13,13 @@ use app\models\Article;
 			<div class="col col-md-6 article-heading-title-original">
 				<h1>
 					<a href="#"><?= $model->title_original ?></a>
-					<span class="label"><?php echo $article->lang_original->language ?></span>
+					<span class="label"><?php echo $article->lang_original_id?></span>
 				</h1>	
 			</div>
 			<div class="col col-md-6 article-heading-title-translate">
 				<h1>
 					<a href="#"><?= $model->title_translate ?></a>
-					<span class="label"><?php echo $article->lang_transtate->language ?></span>
+					<span class="label"><?php echo $article->lang_transtate_id?></span>
 				</h1>
 			</div>
 		</div>
