aec4b36023172288c9635b4f2a9a2ad0b80a7d99
Vasiliy Dubrovin <DubrovinVA@yandex.ru>
2016-05-19 21:42:40 +0300

    правка /article

--- views/article/_article.php
+++ views/article/_article.php
@@ -10,13 +10,13 @@ use app\models\Article;
 			<div class="col col-md-6 article-heading-title-original">
 				<h1>
 					<a href="#"><?= $model->title_original ?></a>
-					<span class="label"><?php echo $article->lang_original->language ?></span>
+					<span class="label">russian</span>
 				</h1>	
 			</div>
 			<div class="col col-md-6 article-heading-title-translate">
 				<h1>
 					<a href="#"><?= $model->title_translate ?></a>
-					<span class="label"><?php echo $article->lang_transtate->language ?></span>
+					<span class="label">english</span>
 				</h1>
 			</div>
 		</div>
