5e8fa5d60099161d17201d4f0168663b1c6fcdba
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-05-20 00:44:10 +0600

    [fix] исправил представление для списка статей

--- views/article/_article.php
+++ views/article/_article.php
@@ -1,8 +1,13 @@
 <?php
-use yii\helpers\Html;
 use yii\widgets\ListView;
 use app\models\Article;
 
+/**
+ * @var Article $model
+ * @var $key
+ * @var $index
+ * @var ListView $widget
+ */
 ?>
 
 	<div class="article">
@@ -10,13 +15,13 @@ use app\models\Article;
 			<div class="col col-md-6 article-heading-title-original">
 				<h1>
 					<a href="#"><?= $model->title_original ?></a>
-					<span class="label">russian</span>
-				</h1>	
+					<span class="label"><?php echo $model->langOriginal->language ?></span>
+				</h1>
 			</div>
 			<div class="col col-md-6 article-heading-title-translate">
 				<h1>
 					<a href="#"><?= $model->title_translate ?></a>
-					<span class="label">english</span>
+					<span class="label"><?php echo $model->langTranstate->language ?></span>
 				</h1>
 			</div>
 		</div>
