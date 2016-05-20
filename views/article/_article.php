7d055a38aa1c221b5a4e8bc09c66582da8cee96b
Vasiliy Dubrovin <DubrovinVA@yandex.ru>
2016-05-20 17:48:18 +0300

    исправление /article

--- views/article/_article.php
+++ views/article/_article.php
@@ -2,21 +2,24 @@
 use yii\helpers\Html;
 use yii\widgets\ListView;
 use app\models\Article;
+use app\models\Language;
 
+$article=new Article();
+$language=new \app\models\Language();
 ?>
 
 	<div class="article">
 		<div class="row article-heading">
 			<div class="col col-md-6 article-heading-title-original">
 				<h1>
-					<a href="#"><?= $model->title_original ?></a>
-					<span class="label">russian</span>
-				</h1>	
+					<?=Html::a(Html::encode($model->title_original), ['view', 'id' => $model->id])?>
+					<span class="label"><?php echo $model->langOriginal->language ?></span>
+				</h1>
 			</div>
 			<div class="col col-md-6 article-heading-title-translate">
 				<h1>
-					<a href="#"><?= $model->title_translate ?></a>
-					<span class="label">english</span>
+					<?=Html::a(Html::encode($model->title_translate), ['view', 'id' => $model->id])?>
+					<span class="label"><?php echo $model->langTranstate->language ?></span>
 				</h1>
 			</div>
 		</div>
