418f82f082b3481f86dd56ed92ad84c2f5e6c388
Vasiliy Dubrovin <DubrovinVA@yandex.ru>
2016-05-22 21:02:52 +0300

    Редактирование /article/

--- views/article/_article.php
+++ views/article/_article.php
@@ -4,9 +4,6 @@ use app\models\Article;
 use app\models\Language;
 use yii\helpers\Html;
 
-$article=new Article();
-$language=new \app\models\Language();
-
 /**
  * @var Article $model
  * @var $key
@@ -14,17 +11,37 @@ $language=new \app\models\Language();
  * @var ListView $widget
  */
 
- ?>
+$page = $widget->dataProvider->getPagination()->getPage() + 1;
+?>
+<?php if (1== $page) { ?>
 
 	<div class="article">
 		<div class="row article-heading">
+			<div class="col col-md-6 article-heading-title-translate">
+				<h1><a href="#"><?php echo $model->title_translate ?></a>
+					<span class="label"><?php echo $model->langTranstate->language ?></span>
+				</h1>
+				<p class="author">Перевод <?php echo $model->translator_name ?></p>
+			</div>
 			<div class="col col-md-6 article-heading-title-original">
+				<h1><a href="#"><?php echo $model->title_original ?></a>
+					<span class="label"><?php echo $model->langOriginal->language ?></span>
+				</h1>
+				<p class="author">By <?php echo $model->author_name ?></p>
+			</div>
+		</div>
+	</div>
+
+<?php } else { ?>
+	<div class="article">
+		<div class="row article-heading">
+			<div class="col col-md-5 article-heading-title-original">
 				<h1>
 					<?=Html::a(Html::encode($model->title_original), ['view', 'id' => $model->id])?>
 					<span class="label"><?php echo $model->langOriginal->language ?></span>
 				</h1>
 			</div>
-			<div class="col col-md-6 article-heading-title-translate">
+			<div class="col col-md-5 article-heading-title-translate">
 				<h1>
 					<?=Html::a(Html::encode($model->title_translate), ['view', 'id' => $model->id])?>
 					<span class="label"><?php echo $model->langTranstate->language ?></span>
@@ -32,9 +49,14 @@ $language=new \app\models\Language();
 			</div>
 		</div>
 	</div>
-	<nav>
-        <ul class="pager">
-            <li class="previous"><a href="/prototype/articles/list?page=1"><span aria-hidden="true">&larr;</span> Newer</a></li>
-            <li class="next"><a href="/prototype/articles/list?page=2">Older <span aria-hidden="true">&rarr;</span></a></li>
-        </ul>
-    </nav>
+<?php } ?>
+<!--
+<nav>
+	<ul class="pager">
+		<li class="previous"><a href="/prototype/articles/list?page=1"><span aria-hidden="true">&larr;</span> Newer</a></li>
+		<li class="next"><a href="/prototype/articles/list?page=2">Older <span aria-hidden="true">&rarr;</span></a></li>
+	</ul>
+</nav>
+-->
+
+	
