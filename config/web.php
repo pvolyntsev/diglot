c2cce9b704dec7a16b7fa67f0f35fa0ccb918096
molchanovvg <valeruko@gmail.com>
2016-05-23 06:41:47 +0300

    [add] add component ElasticSearch in config file Добавил в web.php, раздел component, настройку для ElasticSearch

--- config/web.php
+++ config/web.php
@@ -95,6 +95,13 @@ $config = [
             ],
         ],
         'db' => $db,
+        'elasticsearch' => [
+            'class' => 'yii\elasticsearch\Connection',
+            'nodes' => [
+                ['http_address' => '127.0.0.1:9200'],
+                // configure more hosts if you have a cluster
+            ],
+        ],
     ],
 	'modules' => [
 		'user' => [
