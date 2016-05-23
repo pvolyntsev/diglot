0029ad1ff903c577ed1c0e0b00ef4a121488f99d
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-05-23 12:38:12 +0600

    [change] изменил вёрстку прототипа страницы комментариев - удалил комментарии

--- views/prototype/article-comments.php
+++ views/prototype/article-comments.php
@@ -78,30 +78,14 @@ $model=new \app\models\Comment();
     </div>
     <?php } ?>
 
-<!--    Вставим поле для добавления нового комментария-->
-
+        <!--    Поле для добавления нового комментария-->
         <div class="comment-form">
-
             <?php $form = ActiveForm::begin(['action' =>['prototype/article/comments'], 'id' => 'forum_post', 'method' => 'post',]); ?>
-
-            <!--        --><?//= $form->field($model, 'user_id')->textInput() ?>
-
-<!--            --><?//= $form->field($model, 'article_id')->hiddenInput() ?>
-
             <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
-
-            <!--        --><?//= $form->field($model, 'date_created')->textInput() ?>
-
-            <!--        --><?//= form->field($model, 'date_modified')->textInput() ?>
-
-            <!--        --><?//= $form->field($model, 'status')->dropDownList([ 'published' => 'Published', 'blocked' => 'Blocked', ], ['prompt' => '']) ?>
-
             <div class="form-group">
                 <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
             </div>
-
             <?php ActiveForm::end(); ?>
-
         </div>
 
     <div id="js-comments-recommended">
