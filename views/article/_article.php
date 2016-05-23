af89bd3ce504c04ac24c1c87169aa912a7e3177f
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-05-23 12:27:50 +0600

    [change] вёрстка списка статей
    1. первая страница - вывод во всю ширину экрана, заголовки и два абзаца
    2. вторая и последующие страницы - разбивка экрана наполовину + заголовки обрезанные + один абзац обрезанный

--- views/article/_article.php
+++ views/article/_article.php
@@ -9,54 +9,56 @@ use yii\helpers\Html;
  * @var $key
  * @var $index
  * @var ListView $widget
+ * @var int $count
+ * @var int $page
  */
 
-$page = $widget->dataProvider->getPagination()->getPage() + 1;
+$paragraphs = (1 == $page) ? array_slice($model->paragraphs, 0, 2) : array_slice($model->paragraphs, 0, 1);
 ?>
-<?php if (1== $page) { ?>
+    <div class="row article-heading">
+        <div class="col col-md-6 article-heading-title-translate">
+            <h1>
+                <?php if (1 == $page) { ?>
+                    <p><?=Html::a(Html::encode($model->title_translate), ['view', 'id' => $model->id])?></p>
+                <?php } else { ?>
+                    <p title="<?php echo Html::encode($model->title_translate) ?>"><?=Html::a(Html::encode(mb_substr($model->title_translate, 0, 40, 'utf-8')).'...', ['view', 'id' => $model->id])?></p>
+                <?php } ?>
+                <span class="label"><?php echo $model->langTranslate->language ?></span>
+            </h1>
+            <p class="author">Перевод <?php echo $model->translator_name ?></p>
+        </div>
+        <div class="col col-md-6 article-heading-title-original">
+            <h1>
+                <?php if (1 == $page) { ?>
+                    <p><?=Html::a(Html::encode($model->title_original), ['view', 'id' => $model->id])?></p>
+                <?php } else { ?>
+                    <p title="<?php echo Html::encode($model->title_original) ?>"><?=Html::a(Html::encode(mb_substr($model->title_original, 0, 40, 'utf-8')).'...', ['view', 'id' => $model->id])?></p>
+                <?php } ?>
+                <span class="label"><?php echo $model->langOriginal->language ?></span>
+            </h1>
+            <p class="author">By <?php echo $model->author_name ?></p>
+        </div>
+    </div>
 
-	<div class="article">
-		<div class="row article-heading">
-			<div class="col col-md-6 article-heading-title-translate">
-				<h1><a href="#"><?php echo $model->title_translate ?></a>
-					<span class="label"><?php echo $model->langTranslate->language ?></span>
-				</h1>
-				<p class="author">Перевод <?php echo $model->translator_name ?></p>
-			</div>
-			<div class="col col-md-6 article-heading-title-original">
-				<h1><a href="#"><?php echo $model->title_original ?></a>
-					<span class="label"><?php echo $model->langOriginal->language ?></span>
-				</h1>
-				<p class="author">By <?php echo $model->author_name ?></p>
-			</div>
-		</div>
-	</div>
+    <?php foreach($paragraphs as $paragraph) {?>
+        <div class="row article-paragraph">
+            <div class="col col-md-6 article-paragraph-translate">
+                <?php if (1 == $page) { ?>
+                    <p><?=Html::a(Html::encode($paragraph->paragraph_translate), ['view', 'id' => $model->id])?></p>
+                <?php } else { ?>
+                    <p><?=Html::a(Html::encode(mb_substr($paragraph->paragraph_translate, 0, 80, 'utf-8')).'...', ['view', 'id' => $model->id])?></p>
+                <?php } ?>
+            </div>
+            <div class="col col-md-6 article-paragraph-original">
+                <?php if (1 == $page) { ?>
+                    <p><?=Html::a(Html::encode($paragraph->paragraph_original), ['view', 'id' => $model->id])?></p>
+                <?php } else { ?>
+                    <p><?=Html::a(Html::encode(mb_substr($paragraph->paragraph_original, 0, 80, 'utf-8')).'...', ['view', 'id' => $model->id])?></p>
+                <?php } ?>
+            </div>
+        </div>
+    <?php } ?>
 
-<?php } else { ?>
-	<div class="article">
-		<div class="row article-heading">
-			<div class="col col-md-5 article-heading-title-original">
-				<h1>
-					<?=Html::a(Html::encode($model->title_original), ['view', 'id' => $model->id])?>
-					<span class="label"><?php echo $model->langOriginal->language ?></span>
-				</h1>
-			</div>
-			<div class="col col-md-5 article-heading-title-translate">
-				<h1>
-					<?=Html::a(Html::encode($model->title_translate), ['view', 'id' => $model->id])?>
-					<span class="label"><?php echo $model->langTranslate->language ?></span>
-				</h1>
-			</div>
-		</div>
-	</div>
-<?php } ?>
-<!--
-<nav>
-	<ul class="pager">
-		<li class="previous"><a href="/prototype/articles/list?page=1"><span aria-hidden="true">&larr;</span> Newer</a></li>
-		<li class="next"><a href="/prototype/articles/list?page=2">Older <span aria-hidden="true">&rarr;</span></a></li>
-	</ul>
-</nav>
--->
-
-	
+    <div class="article-more">
+        <?=Html::a(Html::encode('. . .'), ['view', 'id' => $model->id])?>
+    </div>
--- views/article/index.php
+++ views/article/index.php
@@ -6,60 +6,51 @@ use yii\widgets\ListView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-
 $this->title = 'Articles';
 $this->params['breadcrumbs'][] = $this->title;
 
-//$page = $widget->dataProvider->getPagination()->getPage() + 1;
 ?>
 
-<div class="article-index">
-    <div class="col col-md-2">
-		<h1><?= Html::encode($this->title) ?></h1>
-		<p>
-			<?= Html::a('Create Article', ['create'], ['class' => 'btn btn-success']) ?>
-		</p>
-	</div>
-	<div class="articles list">
-		
-			<div class="row two-columns">
-				<?= ListView::widget([
-					'dataProvider' => $dataProvider,
-					'itemOptions' => ['class' => 'item'],
-					'itemView' => '_article',
-					// widget settings
-					/*настройки контейнера списка
-					'options' => [
-						'tag' => 'div',
-						'class' => 'news-list',
-						'id' => 'news-list',
-					],
-					*/
-					'layout' => "{pager}\n{summary}\n{items}\n{pager}",
-					'summary' => 'showing {count} of {totalCount} pages',
-					'summaryOptions' => [
-						'tag' => 'span',
-						'class' => 'my-summary'
-					],
-				 
-					'itemOptions' => [
-						'tag' => 'div',
-						'class' => 'news-item',
-					],
-				 
-					'emptyText' => '<p>articles do not exist</p>',
-					'emptyTextOptions' => [
-						'tag' => 'p'
-					],
-				 
-					'pager' => [
-						//'firstPageLabel' => 'Первая',
-						//'lastPageLabel' => 'Последняя',
-						'nextPageLabel' => 'Older &rarr;',
-						'prevPageLabel' => '&larr; Newer',        
-						'maxButtonCount' => 0,
-					],
-				]) ?>
-			</div>
-	</div>
-</div>
+        <?php
+            $dataProvider->prepare();
+            $pageNumber = $dataProvider->getPagination()->getPage()+1;
+            $itemsCount = $dataProvider->getCount();
+        ?>
+        <?= ListView::widget([
+            'dataProvider' => $dataProvider,
+            'itemView' => '_article',
+            'viewParams' => [
+                'page' => $pageNumber,
+                'count' => $itemsCount,
+            ],
+
+            /*настройки контейнера списка */
+            'options' => [
+                'tag' => 'div',
+                'class' => 'articles list' . ($pageNumber > 1 ? ' row two-columns' : '') ,
+                'id' => 'articles-list',
+            ],
+
+            /*настройки элемента списка */
+            'itemOptions' => [
+                'tag' => 'div',
+                'class' => 'article' . ($pageNumber > 1 ? ' col col-md-6' : ''),
+            ],
+
+            'layout' => $pageNumber == 1 ? "<nav>{pager}</nav>\n{items}\n<nav>{pager}</nav>" : "{items}\n<nav>{pager}</nav>",
+
+            'emptyText' => 'No articles to show',
+            'emptyTextOptions' => [
+                'tag' => 'p'
+            ],
+
+            /* настройки постраничной навигации */
+            'pager' => [
+                'options' => [ 'class' => 'pager' ],
+                'nextPageCssClass' => 'next',
+                'nextPageLabel' => 'Older &rarr;',
+                'prevPageCssClass' => 'previous',
+                'prevPageLabel' => '&larr; Newer',
+                'maxButtonCount' => 0,
+            ],
+        ]) ?>
