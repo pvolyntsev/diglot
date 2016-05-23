eedb8d8be7c84b34afb384c25ee268f87cf76a29
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-05-23 10:02:53 +0600

    [delete] далён старый код

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,7 +3,6 @@
 namespace app\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\forms\ContactForm;
@@ -13,17 +12,6 @@ class SiteController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
