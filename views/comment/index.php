8d677937a231d9f9b4446e5f31628d31bb66572a
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-05-27 02:52:14 +0600

    [added] панель администратора и два контролера - для комментариев и для баннеров

--- views/comment/index.php
+++ views/comment/index.php
@@ -4,21 +4,23 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 /* @var $this yii\web\View */
+/* @var $searchModel app\forms\CommentForm */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Comments';
+$this->title = Yii::t('app', 'Comments');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="comment-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Comment', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create Comment'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-<?php Pjax::begin(['id'=>'comments_list']); ?>
-    <?= GridView::widget([
+<?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
---
+++ views/layouts/admin.php
@@ -0,0 +1,92 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AdminAppAsset;
+
+AdminAppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+
+    <?= Html::csrfMetaTags() ?>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<?php
+NavBar::begin([
+    'brandLabel' => Yii::$app->params['name'],
+    'brandUrl' => Yii::$app->homeUrl,
+    'options' => [
+        'class' => 'navbar navbar-inverse navbar-fixed-top',
+    ],
+    'innerContainerOptions' => [
+        'class' => 'container-fluid'
+    ]
+]);
+
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav'],
+    'items' => [
+        //['label' => 'Add Article', 'url' => ['/article/create'], ],
+    ]
+]);
+
+if (Yii::$app->user->isGuest)
+{
+    $items[] = ['label' => 'Login', 'url' => ['/login'],  ];
+    $items[] = ['label' => 'Signup', 'url' => ['/signup'],  ];
+} else {
+    $items[] = ['label' => 'Profile', 'url' => ['/profile'],  ];
+    $items[] = '<li>'
+        . Html::beginForm(['/logout'], 'post', ['class' => 'navbar-form'])
+        . Html::submitButton(
+            'Logout', // (' . Yii::$app->user->identity->username . ')
+            ['class' => 'btn btn-link']
+        )
+        . Html::endForm()
+        . '</li>';
+}
+
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav navbar-right'],
+    'items' => $items,
+]);
+NavBar::end();
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                <li><?php echo Html::a("Banners", ['/banner']) ?></li>
+                <li><?php echo Html::a("Comments", ['/comment']) ?></li>
+            </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <?php echo Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= $content ?>
+        </div>
+    </div>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
