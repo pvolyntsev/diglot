50d63f535260c3115c85bbff4feb5300f304dbcb
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-06-08 01:47:49 +0600

    [change] форматирование кода
    поменял выражение для формирования action формы

--- views/article/_AddingCommentForm.php
+++ views/article/_AddingCommentForm.php
@@ -9,11 +9,16 @@ Pjax::begin(['id'=>'new_note',
     'enablePushState' => false,
     'enableReplaceState' => false
 ]);
-$form = ActiveForm::begin([ 'action' => 'add-comment?id='.$model->id,'id' => 'addComment','enableClientValidation'=>false,'enableAjaxValidation' => true,'options' => ['data-pjax' => true,]]) ?>
-
-
-<?= $form->field($comment, 'comment')->textarea(['rows' => 6])?>
-
+$form = ActiveForm::begin([
+    'action' => [ 'add-comment', 'id' => $model->id ],
+    'id' => 'addComment',
+    'enableClientValidation' => false,
+    'enableAjaxValidation' => true,
+    'options' => [
+        'data-pjax' => true,
+    ]]);
+?>
+    <?= $form->field($comment, 'comment')->textarea(['rows' => 6])?>
     <div class="controls form-group">
         <?php echo Html::submitButton($comment->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Create'), ['class' => $comment->isNewRecord ? 'btn btn-success' : 'btn btn-primary','id' => 'btn_create','value' => 'add-comment?id='.$model->id]); ?>
     </div>
