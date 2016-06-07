ff21b9634c27aa34943cd6f0ca6a2a96947db8f8
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-06-08 03:17:09 +0600

    [change] поправил класс модели Article

--- models/Article.php
+++ models/Article.php
@@ -27,9 +27,9 @@ use Yii;
  * @property integer $lang_original_id
  * @property integer $lang_translate_id
  *
- * @property Language $langTranslate
  * @property User $user
  * @property Language $langOriginal
+ * @property Language $langTranslate
  * @property CategoryOfArticle[] $categoryOfArticles
  * @property Category[] $categories
  * @property Comment[] $comments
@@ -53,16 +53,16 @@ class Article extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['title_original', 'title_translate', 'user_id', 'lang_original_id'], 'required'],
+            [['title_original', 'user_id', 'lang_original_id'], 'required'],
             [['status'], 'string'],
             [['date_created', 'date_modified', 'date_deleted', 'date_published'], 'safe'],
             [['user_id', 'own_original', 'own_translate', 'lang_original_id', 'lang_translate_id'], 'integer'],
             [['title_original', 'title_translate'], 'string', 'max' => 100],
             [['url_original', 'url_translate', 'author_url', 'translator_url'], 'string', 'max' => 500],
             [['author_name', 'translator_name'], 'string', 'max' => 255],
-            [['lang_translate_id'], 'exist', 'skipOnError' => true, 'targetClass' => Language::className(), 'targetAttribute' => ['lang_translate_id' => 'id']],
             [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
             [['lang_original_id'], 'exist', 'skipOnError' => true, 'targetClass' => Language::className(), 'targetAttribute' => ['lang_original_id' => 'id']],
+            [['lang_translate_id'], 'exist', 'skipOnError' => true, 'targetClass' => Language::className(), 'targetAttribute' => ['lang_translate_id' => 'id']],
         ];
     }
 
@@ -72,25 +72,25 @@ class Article extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'title_original' => 'Title Original',
-            'url_original' => 'Url Original',
-            'title_translate' => 'Title Translate',
-            'url_translate' => 'Url Translate',
-            'status' => 'Статуc',
-            'date_created' => 'Date Created',
-            'date_modified' => 'Date Modified',
-            'date_deleted' => 'Date Deleted',
-            'date_published' => 'Date Published',
-            'user_id' => 'User ID',
-            'author_name' => 'Author Name',
-            'author_url' => 'Author Url',
-            'own_original' => 'Собственная оригинальная статья',
-            'translator_name' => 'Translator Name',
-            'translator_url' => 'Translator Url',
-            'own_translate' => 'Собственный перевод',
-            'lang_original_id' => 'Язык оригинальной статьи',
-            'lang_translate_id' => 'Язык перевода',
+            'id' => Yii::t('app', 'ID'),
+            'title_original' => Yii::t('app', 'Title Original'),
+            'url_original' => Yii::t('app', 'Url Original'),
+            'title_translate' => Yii::t('app', 'Title Translate'),
+            'url_translate' => Yii::t('app', 'Url Translate'),
+            'status' => Yii::t('app', 'Статуc'),
+            'date_created' => Yii::t('app', 'Date Created'),
+            'date_modified' => Yii::t('app', 'Date Modified'),
+            'date_deleted' => Yii::t('app', 'Date Deleted'),
+            'date_published' => Yii::t('app', 'Date Published'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'author_name' => Yii::t('app', 'Author Name'),
+            'author_url' => Yii::t('app', 'Author Url'),
+            'own_original' => Yii::t('app', 'Own Original Article'),
+            'translator_name' => Yii::t('app', 'Translator Name'),
+            'translator_url' => Yii::t('app', 'Translator Url'),
+            'own_translate' => Yii::t('app', 'Собственный перевод'),
+            'lang_original_id' => Yii::t('app', 'Original Language'),
+            'lang_translate_id' => Yii::t('app', 'Translation Language'),
         ];
     }
 
@@ -155,7 +155,7 @@ class Article extends \yii\db\ActiveRecord
      */
     public function getParagraphs()
     {
-        return $this->hasMany(Paragraph::className(), ['article_id' => 'id'])->orderBy('id');
+        return $this->hasMany(Paragraph::className(), ['article_id' => 'id'])->orderBy('sortorder');
     }
 
     /**
