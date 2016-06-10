bbd26f01392052385d8f2fdb73e21f1e7ff60b4a
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2016-06-11 02:17:55 +0600

    [changed] Уведомление о добавлении комментария

--- views/article/_AddingCommentForm.php
+++ views/article/_AddingCommentForm.php
@@ -2,7 +2,9 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\widgets\Pjax;
+use yii\bootstrap\Alert;
 
+/* @var $this \yii\web\View */
 /* @var $article app\models\Article */
 /* @var $comment app\models\Comment */
 /* @var $added bool */
@@ -11,8 +13,6 @@ Pjax::begin(['id'=>'new_comment_form',
     'enablePushState' => false,
     'enableReplaceState' => false
 ]);
-if (!empty($added))
-    echo '<div>', Yii::t('app', 'Response published'), '</div>';
 
 $form = ActiveForm::begin([
     'action' => [ 'add-comment', 'id' => $article->id ],
@@ -26,8 +26,30 @@ $form = ActiveForm::begin([
     <?= $form->field($comment, 'comment')->textarea(['rows' => 6])?>
     <div class="controls form-group">
         <?php echo Html::submitButton($comment->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Create'), ['class' => $comment->isNewRecord ? 'btn btn-success' : 'btn btn-primary','id' => 'btn_create','value' => 'add-comment?id='.$article->id]); ?>
-    </div>
+        <?php
 
-<?php ActiveForm::end();
+        if (!empty($added)) {
+            $id = 'js-alert-'.time();
+            echo Alert::widget([
+                'id' => $id,
+                'options' => [
+                    'class' => 'alert-info float right',
+                ],
+                'closeButton' => false,
+                'body' => Yii::t('app', 'Response added. Thank you!'),
+            ]);
+
+            $this->registerJs(<<<JS
+jQuery(function(){
+    setTimeout(function(){
+        jQuery('#$id').fadeOut('slow').remove();
+    }, 1500);
+});
+JS
+            );
+        }
+        ?>
+    </div>
+<?php
+ActiveForm::end();
 Pjax::end();
-?>
