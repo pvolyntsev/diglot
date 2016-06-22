5afb9f162d41047a6141a0368862292f2dcd9d09
molchanovvg <valeruko@gmail.com>
2016-06-22 09:06:27 +0300

    [new] языковой ресурс EN

---
+++ messages/en-US/app.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'ADD_ARTICLE' => 'Add Article',
+    'LOGIN' => 'Login',
+    'SIGNUP' => 'Signup',
+    'DONATE' => 'Donate',
+    'LANG' => 'Lang',
+    'SEARCH___' => 'search...',
+    'PUBLISH_NEW_ARTICLE_TRANSLATE' => 'Publish new Article or Translation',
+    'ABOUT_DIGLOT_SERVICE' => 'About Diglot Service',
+    'TEAM_BEHIND_SERVICE' => 'Team Behind Service',
+    'DONATE' => 'Donate',
+
+
+];
