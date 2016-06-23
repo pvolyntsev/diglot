4a300b3dca73c0550b7f9c1d3e4dcabc0dc9c416
molchanovvg <valeruko@gmail.com>
2016-06-23 08:39:39 +0300

    [change] Перевод интерфейса

--- messages/ru/app.php
+++ messages/ru/app.php
@@ -10,7 +10,8 @@ return [
     'PUBLISH_NEW_ARTICLE_TRANSLATE' => 'Опубликовать статью или перевод',
     'ABOUT_DIGLOT_SERVICE' => 'Что такое Diglot',
     'TEAM_BEHIND_SERVICE' => 'Команда',
-    'DONATE' => 'Пожертвование',
+    'SUPPORT_US' => 'Поддержите нас',
+    'HELP_US' => 'Помогите нам',
 
 
 ];
