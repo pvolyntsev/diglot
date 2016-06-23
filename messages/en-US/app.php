ca943207f67ef09dc7a34ce181788445029c10ea
molchanovvg <valeruko@gmail.com>
2016-06-23 08:41:13 +0300

    [refactor] Обновлены фразы для перевода

--- messages/en-US/app.php
+++ messages/en-US/app.php
@@ -10,7 +10,8 @@ return [
     'PUBLISH_NEW_ARTICLE_TRANSLATE' => 'Publish new Article or Translation',
     'ABOUT_DIGLOT_SERVICE' => 'About Diglot Service',
     'TEAM_BEHIND_SERVICE' => 'Team Behind Service',
-    'DONATE' => 'Donate',
+    'SUPPORT_US' => 'Support us',
+    'HELP_US' => 'Help us',
 
 
 ];
